b80e0301824ba2f548438c61110e5b80b0c0b2a1
Maarten Everts <maarten.everts@tno.nl>
2015-12-17 21:25:00 +0100

    Preparation for 0.2.0 release.

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 setup(name='pynrrd',
-      version='0.1',
-      description='Pure python module for reading nrrd files.',
+      version='0.2.0',
+      description='Pure python module for reading and writing nrrd files.',
+      long_description='Pure python module for reading and writing nrrd files. See the github page for more information.',
       author='Maarten Everts',
-      author_email='maarten@bitpuzzle.com',
+      author_email='me@nn8.nl',
       url='http://github.com/mhe/pynrrd',
       py_modules=['nrrd'],
+      license="MIT License",
       install_requires=['numpy'],
+      keywords = ["nrrd"],
+      classifiers = [
+          "License :: OSI Approved :: MIT License",
+          "Topic :: Scientific/Engineering",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3"
+      ]
       )
