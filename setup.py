430265dbfb796b4e58096ce22e1bdf8aa4a38ad5
Maarten Everts <maarten.everts@tno.nl>
2017-12-25 12:34:33 +0100

    Version bump to 0.2.4.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='pynrrd',
-      version='0.2.3',
+      version='0.2.4',
       description='Pure python module for reading and writing nrrd files.',
       long_description='Pure python module for reading and writing nrrd files. See the github page for more information.',
       author='Maarten Everts',
@@ -13,11 +13,11 @@ setup(name='pynrrd',
       py_modules=['nrrd'],
       license="MIT License",
       install_requires=['numpy'],
-      keywords = ["nrrd"],
-      classifiers = [
+      keywords=["nrrd"],
+      classifiers=[
           "License :: OSI Approved :: MIT License",
           "Topic :: Scientific/Engineering",
           "Programming Language :: Python",
           "Programming Language :: Python :: 3"
       ]
-      )
+     )
