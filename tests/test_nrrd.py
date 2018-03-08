36acd05089fd8c6dc13cc54a97a4c2e2a6bf2ba6
Simon Pezold <simon.pezold@unibas.ch>
2018-03-08 11:57:27 +0100

    tests running in Python 2.X again

--- tests/test_nrrd.py
+++ tests/test_nrrd.py
@@ -47,7 +47,11 @@ class TestReadingFunctions(unittest.TestCase):
         self.assertEqual(self.expected_header, header)
 
     def test_read_detached_header_only_filename(self):
-        with self.assertRaisesRegex(nrrd.NrrdError, 'Missing magic "NRRD" word. Is this an NRRD file\?'):
+        try:
+            assertRaisesRegex = self.assertRaisesRegex
+        except AttributeError:
+            assertRaisesRegex = self.assertRaisesRegexp
+        with assertRaisesRegex(nrrd.NrrdError, 'Missing magic "NRRD" word. Is this an NRRD file\?'):
             nrrd.read_header(RAW_NHDR_FILE_PATH)
 
     def test_read_header_and_data_filename(self):
