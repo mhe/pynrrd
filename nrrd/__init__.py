59246bc6f402181aa2d1021da95d7fe24e86eab9
Addison Elliott <addison.elliott@gmail.com>
2018-08-16 11:11:51 -0500

    Fix nits

--- nrrd/__init__.py
+++ nrrd/__init__.py
@@ -2,6 +2,7 @@ from nrrd.formatters import *
 from nrrd.parsers import *
 from nrrd.reader import read, read_data, read_header
 from nrrd.writer import write
+from _version import __version__
 
 __all__ = ['read', 'read_data', 'read_header', 'write', 'format_number_list', 'format_number', 'format_matrix',
            'format_optional_matrix', 'format_optional_vector', 'format_vector', 'parse_matrix',
