0bd57f1729b15fd7fb92ab4c0fcf7ecab8c8e6c9
Addison Elliott <addison.elliott@gmail.com>
2018-08-15 22:55:04 -0500

    Set minimum version for Numpy library to 1.11.1
    This was experimentally found to be a version of Numpy that successfully runs the pynrrd tests. It is likely that earlier versions of Numpy may work but the cutoff was just set here.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(name='pynrrd',
       url='https://github.com/mhe/pynrrd',
       py_modules=['nrrd'],
       license='MIT License',
-      install_requires=['numpy'],
+      install_requires=['numpy>=1.11.1'],
       keywords='nrrd teem image processing file format',
       classifiers=[
           'License :: OSI Approved :: MIT License',
