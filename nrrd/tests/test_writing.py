106d2f794ed2ea009009b50d9e9ad188d2c78c71
Billah, Tashrif <tbillah@bwh.harvard.edu>
2019-01-24 21:33:04 -0500

    relative path test

--- nrrd/tests/test_writing.py
+++ nrrd/tests/test_writing.py
@@ -158,7 +158,7 @@ class TestWritingFunctions(unittest.TestCase):
         output_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.nhdr')
         output_data_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.nrrd')
 
-        nrrd.write(output_data_filename, self.data_input, {u'encoding': 'raw'}, detached_header=True)
+        nrrd.write(output_data_filename, self.data_input, {u'encoding': 'raw'}, detached_header=True, relative_data_path= False)
 
         # Read back the same file
         data, header = nrrd.read(output_filename)
@@ -198,7 +198,7 @@ class TestWritingFunctions(unittest.TestCase):
         output_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.nhdr')
         output_data_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.raw.gz')
 
-        nrrd.write(output_filename, self.data_input, {u'encoding': 'gz'}, detached_header=False)
+        nrrd.write(output_filename, self.data_input, {u'encoding': 'gz'}, detached_header=False, relative_data_path=False)
 
         # Read back the same file
         data, header = nrrd.read(output_filename)
@@ -216,7 +216,7 @@ class TestWritingFunctions(unittest.TestCase):
         data, header = nrrd.read(output_filename)
         self.assertEqual(self.expected_data, data.tostring(order='F'))
         self.assertEqual(header['encoding'], 'bz2')
-        self.assertEqual(header['data file'], output_data_filename)
+        self.assertEqual(header['data file'], os.path.basename(output_data_filename))
 
     def test_write_detached_ascii(self):
         output_filename = os.path.join(self.temp_write_dir, 'testfile_detached_raw.nhdr')
@@ -228,7 +228,7 @@ class TestWritingFunctions(unittest.TestCase):
         data, header = nrrd.read(output_filename)
         self.assertEqual(self.expected_data, data.tostring(order='F'))
         self.assertEqual(header['encoding'], 'txt')
-        self.assertEqual(header['data file'], output_data_filename)
+        self.assertEqual(header['data file'], os.path.basename(output_data_filename))
 
 
 if __name__ == '__main__':
