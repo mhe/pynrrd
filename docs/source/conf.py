1307d476065e6ebda5896f8667d563f6acca79d6
Addison Elliott <addison.elliott@gmail.com>
2022-09-20 18:48:08 -0500

    refactor config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,11 +4,6 @@ sys.path.insert(0, os.path.abspath('../../'))
 import nrrd
 
 # -- General configuration ------------------------------------------------
-
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
