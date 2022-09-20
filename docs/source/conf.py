8c1cf70a48406332b3656211bb5331054993e17a
Addison Elliott <addison.elliott@gmail.com>
2022-09-20 18:56:58 -0500

    Remove html_sidebars config option

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,21 +83,6 @@ html_css_files = ['css/custom.css']
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# This is required for the alabaster theme
-# refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-        'donate.html',
-    ]
-}
-
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
